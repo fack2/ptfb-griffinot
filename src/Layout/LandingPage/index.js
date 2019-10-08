9d66963573eb4998b7c5c8405e5bcaeae95dc6a7
sara219 <whitenoise6014@gmail.com>
2019-10-08 23:00:51 +0300

    create the landing page by added the title, image, intro and the starting button

---
+++ src/Layout/LandingPage/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom'
+import { Title, LandingImg, Paragraph, Button, ResourceLink, Link } from './index.style';
+import landing from '../../assets/landing-img.jpeg';
+import Header from '../../CommonComponents/Header';
+
+class LandingPage extends React.Component {
+
+  state = {
+    redirect: false,
+    render: false
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  renderBtn = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/scheme' />
+    }
+  }
+
+
+
+  setRender = () => {
+    this.setState({
+      render: true
+    })
+  }
+
+  renderLink = () => {
+    if (this.state.render) {
+      return <Redirect to='/resource' />
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Title>Supporting you with your child’s journey </Title>
+        <LandingImg alt="landing" src={landing} />
+        <Paragraph>
+          Griffin Occupational Therapy provides online training and programmes for parents and teachers of children with motor skill delays and sensory processing challenges. We offer affordables online supports which teach you how to help your child.
+        </Paragraph>
+
+        {this.renderBtn()}
+        <Button onClick={this.setRedirect} type="button">Get Started</Button>
+
+        {this.renderLink()}
+        <ResourceLink>
+          View our resources page
+          <Link onClick={this.setRender}> here</Link>
+        </ResourceLink>
+      </>
+    );
+  }
+}
+
+export default LandingPage;
