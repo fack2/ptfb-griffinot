06030221946ca704074ffc264c9e4455dc806108
sara219 <whitenoise6014@gmail.com>
2019-10-08 23:02:27 +0300

    added the style to the landing page using styled-component #27

---
+++ src/Layout/LandingPage/index.style.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 5rem;
+`;
+
+export const LandingImg = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 88rem;
+  height: 72rem;
+  border-radius: 7rem;
+`;
+
+export const Paragraph = styled.p`
+font-size: 1.7rem;
+color: #5C5B5B;
+margin: 6rem 7rem 0rem 7rem;
+`;
+
+export const Button = styled.button`
+display: block;
+margin-left: auto;
+margin-right: auto;
+background-color: #205C8B;
+border: 1px solid #205C8B;
+color: white;
+border-radius: 20rem;
+width: 61rem;
+height: 11rem;
+margin-top: 7rem;
+
+
+font-size: 4.8rem;
+font-weight: bolder;
+`;
+
+export const ResourceLink = styled.p`
+font-size: 3.3rem;
+margin-left: 20rem;
+`;
+
+export const Link = styled.span`
+  color: #205c88;
+`;
