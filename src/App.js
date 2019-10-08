8ed3bedb444241a88421da7cb13644408159bbd4
sara219 <whitenoise6014@gmail.com>
2019-10-08 15:15:55 +0300

    added the NavBar (header) to the app.js #24

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './CommonComponents/Header';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header />
+
     </div>
   );
 }
