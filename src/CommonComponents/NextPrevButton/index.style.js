3f95f046c4a7e336306082ec580f7a60fe750c3b
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-09 11:09:38 +0300

    add style for the next and prev section

--- src/CommonComponents/NextPrevButton/index.style.js
+++ src/CommonComponents/NextPrevButton/index.style.js
@@ -1,37 +1,25 @@
 import styled from 'styled-components'
 
 export const PrevButton = styled.img`
-  position: fixed;
-  bottom: 10.19rem;
   height: 3rem;
   width: 3rem;
-  left: 6rem;
 `
 export const PrevWord = styled.span`
-  position: fixed;
-  left: 10rem;
-  bottom: 9.7rem;
   font-family: Lato;
   font-size: 3.5rem;
   color: var(--next-prev-button-color);
 `
 export const NextButton = styled.img`
-  position: fixed;
-  bottom: 10.2rem;
   height: 3rem;
   width: 3rem;
-  right: 6rem;
 `
 
 export const NextWord = styled.span`
-  position: fixed;
-  right: 10rem;
-  bottom: 9.7rem;
   font-family: Lato;
   font-size: 3.5rem;
   color: var(--next-prev-button-color);
 `
 
-export const Container = styled.div`
-  display: relative;
-`
+// export const Container = styled.div`
+//   display: relative;
+// `
