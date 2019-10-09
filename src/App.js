b5e7079bd3d3d2d2bd9e020c73d76b590894fe74
95Rawan <youlostme17@hotmail.com>
2019-10-09 13:15:26 +0300

    Called Question component in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Question from './CommonComponents/Question';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Question />
     </div>
   );
 }
