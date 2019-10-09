78b12409d607a1740ad68934deb2dbcca89ea150
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-09 12:54:40 +0300

    edit in the style

--- src/CommonComponents/NextPrevButton/index.js
+++ src/CommonComponents/NextPrevButton/index.js
@@ -1,28 +1,17 @@
 import React from 'react'
 import prev from '../../assets/prevButton.png'
 import next from '../../assets/nextButton.png'
-import {
-  PrevButton,
-  PrevWord,
-  NextButton,
-  NextWord,
-  SectionN,
-  SectionP
-} from './index.style'
+import { PrevButton, PrevWord, NextButton, NextWord } from './index.style'
 import './index.style'
 
 export const NextPrevButton = () => {
   return (
     <>
-      <SectionP>
-        <PrevButton alt='PreviousButton' src={prev} />
-        <PrevWord>Prev</PrevWord>
-      </SectionP>
+      <PrevButton alt='PreviousButton' src={prev} />
+      <PrevWord>Prev</PrevWord>
 
-      <SectionN>
-        <NextWord>Next</NextWord>
-        <NextButton alt='NextButton' src={next} />
-      </SectionN>
+      <NextWord>Next</NextWord>
+      <NextButton alt='NextButton' src={next} />
     </>
   )
 }
--- src/CommonComponents/NextPrevButton/index.style.js
+++ src/CommonComponents/NextPrevButton/index.style.js
@@ -1,28 +1,33 @@
 import styled from 'styled-components'
 
 export const PrevButton = styled.img`
+  position: fixed;
+  bottom: 10.2rem;
   height: 3rem;
   width: 3rem;
+  left: 6rem;
 `
 export const PrevWord = styled.span`
+  position: fixed;
+  left: 10rem;
+  bottom: 9.7rem;
   font-family: Lato;
   font-size: 3.5rem;
   color: var(--next-prev-button-color);
 `
 export const NextButton = styled.img`
+  position: fixed;
+  bottom: 10.2rem;
   height: 3rem;
   width: 3rem;
+  right: 6rem;
 `
 
 export const NextWord = styled.span`
+  position: fixed;
+  right: 10rem;
+  bottom: 9.7rem;
   font-family: Lato;
   font-size: 3.5rem;
   color: var(--next-prev-button-color);
-`
-
-export const SectionN = styled.div`
-  margin: -4rem 0 0 74rem;
-`
-export const SectionP = styled.div`
-  margin: 135rem 0 0 10rem;
 `
