a6140ba415e2ebc07f1addf1d51df05254d40f60
sara219 <whitenoise6014@gmail.com>
2019-10-10 09:49:43 +0300

    change the container style to flex-box #32

--- src/Layout/Schema/index.js
+++ src/Layout/Schema/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Title, HorizontalLine, Para1, FineMotorImg, Para2, NextText, NextArrow } from './index.style';
+import { Title, HorizontalLine, Para1, FineMotorImg, Para2, NextText, NextArrow, Container } from './index.style';
 import FMSimg from '../../assets/FMS-schema.jpg'
 import arrow from '../../assets/right-arrow.png'
 
@@ -11,15 +11,17 @@ class Schema extends React.Component {
   render() {
     return (
       <>
-        <Title>Welcome to the GriffinOT Fine Motor Skills programme.</Title>
-        <HorizontalLine />
-        <Para1>Our fine motor skill programme is designed to teach you how to help your child to improve their fine motor skills.<br />
-          <br />
-          The programme is broken into six different sections. There are four levels that support fine motor skill development.  Then, there are two additional sections designed to specifically help with pencil grasp and scissor skills. Each section of the programme is £24 to purchase.  This provides you with 6 months access to the content.  </Para1>
+        <Container>
+          <Title>Welcome to the GriffinOT Fine Motor Skills programme.</Title>
+          <HorizontalLine />
+          <Para1>Our fine motor skill programme is designed to teach you how to help your child to improve their fine motor skills.<br />
+            <br />
+            The programme is broken into six different sections. There are four levels that support fine motor skill development.  Then, there are two additional sections designed to specifically help with pencil grasp and scissor skills. Each section of the programme is £24 to purchase.  This provides you with 6 months access to the content.  </Para1>
 
-        <FineMotorImg alt="FineMotorSkill" src={FMSimg} />
+          <FineMotorImg alt="FineMotorSkill" src={FMSimg} />
 
-        <Para2>Before we can recommend the best section of our programme for your child to start working at, we need to ask you a few questions.</Para2>
+          <Para2>Before we can recommend the best section of our programme for your child to start working at, we need to ask you a few questions.</Para2>
+        </Container>
 
         <NextText type='button'>Start <NextArrow alt='next-arrow' src={arrow} /></NextText>
       </>
