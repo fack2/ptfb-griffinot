2cf2efd4be7caae4781b6be39b251720de9c125b
sara219 <whitenoise6014@gmail.com>
2019-10-10 10:50:27 +0300

    put the box that conten the info in  container #47

--- src/Layout/AboutUs/index.js
+++ src/Layout/AboutUs/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import {
-  Title, Box, KimImg, Name, Para,
+  Title, Box, KimImg, Name, Para, Container,
 } from './index.style';
 
 import KimG from '../../assets/KimG.png';
 
 const AboutUs = () => (
   <>
-    <Title>Your Instructor</Title>
-    <Box>
-      <KimImg alt="Kim-Griffin" src={KimG} />
-      <Name>Kim Griffin</Name>
-      <Para>
-        Kim is an occupational therapist striving to make the occupational therapy knowledge accessible to everyone! Hailing from Australia but now based in London, she splits her time between creating online resources and working in schools.
-        <br />
-        {' '}
-        <br />
-        Kim has completed her post graduate sensory integration training through the University of Ulster and her Masters of Health Science through the University of Sydney. She has an interest in sensory processing, motor skills, autism and attachment.
+    <Container>
+      <Title>Your Instructor</Title>
+      <Box>
+        <KimImg alt="Kim-Griffin" src={KimG} />
+        <Name>Kim Griffin</Name>
+        <Para>
+          Kim is an occupational therapist striving to make the occupational therapy knowledge accessible to everyone! Hailing from Australia but now based in London, she splits her time between creating online resources and working in schools.
+          <br />
+          {' '}
+          <br />
+          Kim has completed her post graduate sensory integration training through the University of Ulster and her Masters of Health Science through the University of Sydney. She has an interest in sensory processing, motor skills, autism and attachment.
 
-      </Para>
-    </Box>
+        </Para>
+      </Box>
+    </Container>
   </>
 );
 
