3802d494d512e745fe12f4e157580d9fdc873b3e
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-10 16:41:48 +0300

    add fake data for resources

---
+++ src/Data/resourceData.js
@@ -0,0 +1,36 @@
+const resourceData = [
+  {
+    picture: '',
+    title: 'Introductory Videos',
+  },
+  {
+    picture: '',
+    title: 'Sensory Training',
+  },
+  {
+    picture: '',
+    title: 'Sensory Introduction',
+  },
+  {
+    picture: '',
+    title: 'Sensory Topics',
+  },
+  {
+    picture: '',
+    title: 'Sensory Equipment',
+  },
+  {
+    picture: 'src/assets/book.jpg',
+    title: 'Books',
+  },
+  {
+    picture: 'src/assets/pgt.jpeg',
+    title: 'Pencil Grasp Tips',
+  },
+  {
+    picture: '',
+    title: 'Pencil Grip Reviews',
+  },
+];
+
+export default resourceData;
