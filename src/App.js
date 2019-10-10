cb1c4dbd57b0741a86e8b0f679e3dd47aaa95933
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-10 11:35:19 +0300

    delete unuse line

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './CommonComponents/Header';
 import LandingPage from './Layout/LandingPage';
 import Schema from './Layout/Schema';
-import NextPrevButton from './CommonComponents/NextPrevButton';
 
 function App() {
   return (
