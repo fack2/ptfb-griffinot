3fc4ed962850b5ba3053944147f2e4a5bbd60e0d
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-13 10:20:23 +0300

    add a PropsType added a props type for prevLink and nextLink

--- src/CommonComponents/NextPrevButton/index.js
+++ src/CommonComponents/NextPrevButton/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import prev from '../../assets/prevButton.png';
@@ -8,7 +9,7 @@ import {
   PrevNextText,
   Container,
   PrevNextItem,
-  LinkButton
+  LinkButton,
 } from './index.style';
 
 const NextPrevButton = ({ prevLink, nextLink }) => (
@@ -16,17 +17,23 @@ const NextPrevButton = ({ prevLink, nextLink }) => (
     <Container>
       <LinkButton to={prevLink}>
         <PrevNextItem>
-          <PrevNextButton alt='PreviousButton' src={prev} />
+          <PrevNextButton alt="PreviousButton" src={prev} />
           <PrevNextText>Prev</PrevNextText>
         </PrevNextItem>
       </LinkButton>
       <LinkButton to={nextLink}>
         <PrevNextItem>
           <PrevNextText>Next</PrevNextText>
-          <PrevNextButton alt='NextButton' src={next} />
+          <PrevNextButton alt="NextButton" src={next} />
         </PrevNextItem>
       </LinkButton>
     </Container>
   </Router>
 );
+
+NextPrevButton.propTypes = {
+  prevLink: PropTypes.string.isRequired,
+  nextLink: PropTypes.string.isRequired,
+};
+
 export default NextPrevButton;
