2f56ac9b643179997934abb87c600c96a5030176
someyaaltous <someyaaltous1995@gmail.com>
2019-10-13 15:02:24 +0300

    added Link to redirect page to griffinot.teachable page

--- src/Layout/Levels/index.js
+++ src/Layout/Levels/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import levelData from '../../Data/levelData';
-import { Title, Bodypage } from './index.style';
+import { Title, Bodypage, Link } from './index.style';
 import Level from '../../CommonComponents/Level';
 
 const Levels = () => (
   <>
     <Title>Here are a courses for all level</Title>
     <Bodypage>
-      {levelData.map(({ levelScore, titleLevel }) => (
-        <Level levelScore={levelScore} titleLevel={titleLevel} />
+      {levelData.map(({ levelScore, titleLevel, link }) => (
+        <Link href={link}>
+          <Level levelScore={levelScore} titleLevel={titleLevel} />
+        </Link>
       ))}
     </Bodypage>
   </>
