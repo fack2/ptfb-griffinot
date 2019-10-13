94ef7d840a52796186a28d274f61fd6c73d8568c
someyaaltous <someyaaltous1995@gmail.com>
2019-10-13 14:13:34 +0300

    add level route in the app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './Layout/LandingPage';
 import Menu from './Layout/Menu';
 import Schema from './Layout/Schema';
 import AboutUs from './Layout/AboutUs';
+import Levels from './Layout/Levels';
 
 function App() {
   return (
@@ -12,15 +13,14 @@ function App() {
       <Router>
         <>
           <Header />
-
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/menu" component={Menu} />
           <Route exact path="/schema" component={Schema} />
           <Route exact path="/about-us" component={AboutUs} />
+          <Route exact PATH="/level" component={Levels} />
         </>
       </Router>
     </>
   );
 }
-
 export default App;
