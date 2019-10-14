3006fe984e97b682ba8532eb118e364880c50cfe
95Rawan <youlostme17@hotmail.com>
2019-10-14 22:11:43 +0300

    Styled sectionA page

---
+++ src/Layout/Questionnaire/index.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Header = styled.h1`
+	text-align: center;
+	margin: 8rem 4rem 4rem 4rem;
+`;
+
+export const Line = styled.hr`
+	border-top: 1px solid var(--main-font-color);
+	margin: 0rem 12rem;
+`;
+
+export const Paragraph = styled.p`
+	font-size: 3.3rem;
+	margin: 8rem 2rem 1rem 3rem;
+	font-weight: bold;
+`;
