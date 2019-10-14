7810c85ccdbd0e7895d7138fdca309a3f12134fe
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-14 15:26:38 +0300

    add the previous button Resources page and make it activate

--- src/Layout/Resources/index.js
+++ src/Layout/Resources/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Resource from '../../CommonComponents/Resource';
+import PrevButton from '../../CommonComponents/PrevButton';
 import { TitleH, Container } from './index.style';
 
-const Resources = () => (
+const Resources = ({ prevLink }) => (
   <Container>
     <TitleH>Helpful resources</TitleH>
     <Resource />
-    <>
+    <PrevButton prevLink="/" />
   </Container>
 );
 
