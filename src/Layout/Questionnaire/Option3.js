1e20366123cfca37307c5560adaccc0a7bdfc5fb
someyaaltous <someyaaltous1995@gmail.com>
2019-10-14 12:17:10 +0300

    added option layout

---
+++ src/Layout/Questionnaire/Option3.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  TitleOption,
+  OptionDiv,
+  ParagraphObtion,
+  DivData,
+  LinkOption,
+} from './index.style';
+import PrevButton from '../../CommonComponents/PrevButton';
+
+const Option3 = () => (
+  <>
+    <TitleOption>
+      What skill do you feel your child needs the most help with improving, at
+      the moment?
+    </TitleOption>
+
+    <DivData>
+      <LinkOption href="/">
+        <OptionDiv>
+          <ParagraphObtion>
+            I would like to help them to improve their pencil grasp
+          </ParagraphObtion>
+        </OptionDiv>
+      </LinkOption>
+      <LinkOption href="/">
+        <OptionDiv>
+          <ParagraphObtion>
+            I would like to help them to improve their general fine motor
+            skills.
+          </ParagraphObtion>
+        </OptionDiv>
+      </LinkOption>
+      <LinkOption href="/">
+        <OptionDiv>
+          <ParagraphObtion>
+            I would like to help them to improve their scissor skills..
+          </ParagraphObtion>
+        </OptionDiv>
+      </LinkOption>
+    </DivData>
+
+    <PrevButton prevLink="/" />
+  </>
+);
+export default Option3;
