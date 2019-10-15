c9a7c8070238b194f64f8db67f55b5ccc32a7eb0
95Rawan <youlostme17@hotmail.com>
2019-10-15 11:41:45 +0300

    Added the second page's endpoint(SectionB) to the app file

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Schema from './Layout/Schema';
 import AboutUs from './Layout/AboutUs';
 import Option3 from './Layout/Questionnaire/Option3';
 import Levels from './Layout/Levels';
-import SectionA from './Layout/Questionnaire/SectionA';
 import Resources from './Layout/Resources';
+import SectionA from './Layout/Questionnaire/SectionA';
+import SectionB from './Layout/Questionnaire/SectionB';
 
 function App() {
   return (
@@ -22,8 +23,9 @@ function App() {
           <Route exact path="/about-us" component={AboutUs} />
           <Route path="/option" component={Option3} />
           <Route path="/levels" component={Levels} />
-          <Route exact path="/questionnaire-section-a" component={SectionA} />
           <Route exact path="/resources" component={Resources} />
+          <Route exact path="/questionnaire-section-a" component={SectionA} />
+          <Route path="/questionnaire-section-b" component={SectionB} />
         </>
       </Router>
     </>
