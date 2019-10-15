d3ef14d90b208c7d424eb6ce35525e2694135fa3
95Rawan <youlostme17@hotmail.com>
2019-10-15 16:02:31 +0300

    Created the third questionnaire page(SectionC)

--- src/Layout/Questionnaire/SectionC.js
+++ src/Layout/Questionnaire/SectionC.js
@@ -21,24 +21,28 @@ const SectionA = () => (
         <Checkbox type="checkbox" id={data[10].questionNumber} />
         <QuestionDescription>{data[10].description}</QuestionDescription>
       </QuestionCard>
+
       <QuestionCard>
         <QuestionNumber>{data[11].questionNumber}</QuestionNumber>
         <QuestionText>{data[11].question}</QuestionText>
         <Checkbox type="checkbox" id={data[11].questionNumber} />
         <QuestionDescription>{data[11].description}</QuestionDescription>
       </QuestionCard>
-      <QuestionCard>
+
+      <QuestionCard height="21rem">
         <QuestionNumber>{data[12].questionNumber}</QuestionNumber>
         <QuestionText>{data[12].question}</QuestionText>
         <Checkbox type="checkbox" id={data[12].questionNumber} />
         <QuestionDescription>{data[12].description}</QuestionDescription>
       </QuestionCard>
+
       <QuestionCard>
         <QuestionNumber>{data[13].questionNumber}</QuestionNumber>
         <QuestionText>{data[13].question}</QuestionText>
         <Checkbox type="checkbox" id={data[13].questionNumber} />
         <QuestionDescription>{data[13].description}</QuestionDescription>
       </QuestionCard>
+
       <QuestionCard>
         <QuestionNumber>{data[14].questionNumber}</QuestionNumber>
         <QuestionText>{data[14].question}</QuestionText>
@@ -46,7 +50,7 @@ const SectionA = () => (
         <QuestionDescription>{data[14].description}</QuestionDescription>
       </QuestionCard>
     </QuestionContainer>
-    <NextPrevButton prevLink="/" nextLink="/" />
+    <NextPrevButton prevLink="/questionnaire-section-b" nextLink="/questionnaire-section-d" />
   </>
 );
 
