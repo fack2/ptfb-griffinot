293714039f78f492e5d6cd428fdb600aa31fe242
sara219 <whitenoise6014@gmail.com>
2019-10-15 09:03:42 +0300

    Resolve comments and fix problems ! #25

--- src/Layout/Schema/index.js
+++ src/Layout/Schema/index.js
@@ -8,8 +8,7 @@ import {
   LogoImage,
   BodyPage,
   PopUpDiv,
-  YesBtn,
-  NoBtn,
+  OptionBtn,
   ContainerBtn,
   Container2,
 } from './index.style';
@@ -19,19 +18,19 @@ import arrow from '../../assets/right-arrow.png';
 class Schema extends React.Component {
 
   state = {
-    Yes: false,
-    No: false
+    YesBtn: false,
+    NoBtn: false
   }
 
   onChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   DisplayPop = () => {
-    this.setState({ No: true })
-  }
+    this.setState({ NoBtn: true }, () => {
+      setTimeout(() => { this.setState({ NoBtn: false }) }, 2000);
+    })
+  };
 
   render() {
     return (
@@ -41,44 +40,36 @@ class Schema extends React.Component {
           <HorizontalLine />
           <Para1>
             Our fine motor skill programme is designed to teach you how to help your child to improve their fine motor skills.
-        <br />
+            <br />
             <br />
             The programme is broken into six different sections. There are four levels that support fine motor skill development.  Then, there are two additional sections designed to specifically help with pencil grasp and scissor skills. Each section of the programme is £24 to purchase.  This provides you with 6 months access to the content.
-      </Para1>
+         </Para1>
           <FineMotorImg alt="FineMotorSkill" src={FMSimg} />
           <Para2>Before we can recommend the best section of our programme for your child to start working at, we need to ask you a few questions.</Para2>
         </Container>
 
 
-        <Popup
-          modal
-          trigger={(
-            <NextText type="button">
-              Start
-          <NextArrow alt="next-arrow" src={arrow} />
-            </NextText>
-          )}
-        >
+        <Popup modal trigger={(<NextText type="button"> Start <NextArrow alt="next-arrow" src={arrow} /> </NextText>)}>
+
           {() => <BodyPage>
             <PopUPCard>
               <PopUpBox Height="80rem">
-                {this.state.No ? <Logo Background='#ABC1D3'>
+                {this.state.NoBtn ? <Logo Background='#ABC1D3'>
                   <LogoImage src={popUpImg} alt="a kid holding a kite logo" />
                 </Logo> : <Logo>
                     <LogoImage src={popUpImg} alt="a kid holding a kite logo" />
                   </Logo>}
                 <Container2>
-                  {this.state.No ? <PopUpDiv> 2. Does your child have a neurological or genetic condition?(e.g. CP, Down's Syndrome)</PopUpDiv> : <PopUpDiv>1. Does your child have a degenerative condition?(e.g. MD,  Rhetts)</PopUpDiv>}
+                  {this.state.NoBtn ? <PopUpDiv> 2. Does your child have a neurological or genetic condition?(e.g. CP, Down's Syndrome)</PopUpDiv> : <PopUpDiv>1. Does your child have a degenerative condition?(e.g. MD,  Rhetts)</PopUpDiv>}
                   <ContainerBtn>
-                    <YesBtn type="button">Yes</YesBtn>
-                    <NoBtn onClick={this.DisplayPop} type="button">No</NoBtn>
+                    <OptionBtn type="button">Yes</OptionBtn>
+                    <OptionBtn onClick={this.DisplayPop} type="button">No</OptionBtn>
                   </ContainerBtn>
                 </Container2>
               </PopUpBox>
             </PopUPCard>
           </BodyPage>}
         </Popup>
-
       </>
     )
   }
--- src/Layout/Schema/index.style.js
+++ src/Layout/Schema/index.style.js
@@ -1,48 +1,48 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-display:flex;
-flex-direction:column;
-justify-content:center;
-overflow: scroll;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  overflow: scroll;
 `;
 
 export const Title = styled.h1`
-text-align:center;
-font-size: 3.5rem;
+  text-align:center;
+  font-size: 3.5rem;
 `;
 
 export const HorizontalLine = styled.hr`
-width: 50rem;
-border: 0.1rem solid #2D2323;
-transform: rotate(180deg);
+  width: 50rem;
+  border: 0.1rem solid #2D2323;
+  transform: rotate(180deg);
 `;
 
 export const Para1 = styled.p`
-font-size: 1.4rem;
-color: #5C5B5B;
-margin: 5rem 1rem 4rem 2rem;
+  font-size: 1.4rem;
+  color: #5C5B5B;
+  margin: 5rem 1rem 4rem 2rem;
 `;
 
 export const FineMotorImg = styled.img`
-margin-left: auto;
-margin-right: auto;
-width: 88rem;
-border: 0.1rem solid #2D2323;
+  margin-left: auto;
+  margin-right: auto;
+  width: 88rem;
+  border: 0.1rem solid #2D2323;
 `;
 
 export const Para2 = styled.p`
-font-size: 1.4rem;
-color: #5C5B5B;
-margin: 5rem 1rem 4rem 2rem;
+  font-size: 1.4rem;
+  color: #5C5B5B;
+  margin: 5rem 1rem 4rem 2rem;
 `;
 
 export const NextText = styled.p`
-font-size: 4rem;
-margin-top: 2rem;
-margin-left: 77rem;
-color: #0F3654;
-font-weight: 900;
+  font-size: 4rem;
+  margin-top: 2rem;
+  margin-left: 77rem;
+  color: #0F3654;
+  font-weight: 900;
 `;
 
 export const NextArrow = styled.img`
@@ -51,12 +51,12 @@ export const NextArrow = styled.img`
 `;
 
 
-export const BodyPage = styled.html`
+export const BodyPage = styled.div`
   background: #cfcfcf;
   justify-content: center;
 `;
 
-export const PopUPCard = styled.body`
+export const PopUPCard = styled.div`
   text-align: center;
   display: flex;
   height: 0.6rem;
@@ -67,25 +67,25 @@ export const PopUpBox = styled.div`
   height: ${(props) => props.Height || '100rem'};
   background: #fffcfc;
   border: 0.1rem solid #c4c4c4;
-    margin-left: -17.4rem;
-    margin-top: -38rem;`;
+  margin-left: -17.4rem;
+  margin-top: -38rem;`;
 
 export const Logo = styled.div`
-    width: 82.5rem;
-    height: 34rem;
-    background: ${(props) => props.Background || '#205c8b'};
-    margin-top: -1rem;
-    margin-left: -0.4rem;
+  width: 82.5rem;
+  height: 34rem;
+  background: ${(props) => props.Background || '#205c8b'};
+  margin-top: -1rem;
+  margin-left: -0.4rem;
 `;
 
 export const LogoImage = styled.img`
-    width: 45rem;
-    height: 41rem;
+  width: 45rem;
+  height: 41rem;
 `;
 
 export const Container2 = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const PopUpDiv = styled.span`
@@ -102,24 +102,13 @@ export const ContainerBtn = styled.div`
 `;
 
 
-export const YesBtn = styled.button`
-    border: 0.3rem #cecece solid;
-    background-color: white;
-    border-radius: 4rem;
-    width: 16rem;
-    height: 7rem;
-    font-size: 3.5rem;
-    outline: none;
-
-`;
-
-export const NoBtn = styled.button`
-    border: 0.3rem #cecece solid;
-    background-color: white;
-    border-radius: 4rem;
-    width: 16rem;
-    height: 7rem;
-    font-size: 3.5rem;
-    outline: none;
+export const OptionBtn = styled.button`
+  border: 0.3rem #cecece solid;
+  background-color: white;
+  border-radius: 4rem;
+  width: 16rem;
+  height: 7rem;
+  font-size: 3.5rem;
+  outline: none;
 
 `;
