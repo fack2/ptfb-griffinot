02d450cb817021d6049e11ce50f32b5e6912d8fb
someyaaltous <someyaaltous1995@gmail.com>
2019-10-16 09:08:08 +0300

    added data of blogs

---
+++ src/CommonComponents/Blog/index.style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const BlogDiv = styled.div`
+  width: 89rem;
+  height: 16rem;
+  border: 0.1rem solid #000000;
+  border-radius: 2rem;
+  margin-bottom: 9.9rem;
+`;
+
+export const ImageBlog = styled.img`
+  margin-top: 4rem;
+  margin-left: 4rem;
+`;
+export const TitleBlog = styled.p`
+     margin-left: 14rem;
+    margin-top: -5.4rem;
+    font-size: 3rem;
+`;
---
+++ src/Data/blogData.js
@@ -0,0 +1,29 @@
+const blogData = [
+  {
+    Title: 'Help! Why is my Child Chewing on Clothing and Other Things?',
+    ImageBlog: 'https://i.imgur.com/sVtJ64N.png',
+    LinkBlog: 'https://www.griffinot.com/child-chewing-clothing/',
+  },
+  {
+    Title:
+      'Why is my Child Still Putting Things in Their Mouth? A Post on Oral Sensory Seeking.',
+    ImageBlog: 'https://i.imgur.com/RYIwIj5.png',
+    LinkBlog: 'https://www.griffinot.com/child-oral-sensory-seeking/',
+  },
+  {
+    Title: 'The Mystery of Heavy Work',
+    ImageBlog: 'https://i.imgur.com/sVtJ64N.png',
+    LinkBlog: 'https://www.griffinot.com/the-mystery-of-heavy-work/',
+  },
+  {
+    Title: 'Is my child dyspraxic?',
+    ImageBlog: 'https://i.imgur.com/RYIwIj5.png',
+    LinkBlog: 'https://www.griffinot.com/is-my-child-dyspraxic/',
+  },
+  {
+    Title: 'Dyspraxia Myths Explained – It’s not just being clumsy!',
+    ImageBlog: 'https://i.imgur.com/sVtJ64N.png',
+    LinkBlog: 'https://www.griffinot.com/dyspraxia-myths-explained/',
+  },
+];
+export default blogData;
