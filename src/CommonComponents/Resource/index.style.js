b762e74ac2398755b9befb4bd0ea177b08926349
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-17 10:06:58 +0300

    edit resource component to make style for book page add a prpos on index.js edit style in index.style.js git status

--- src/CommonComponents/Resource/index.style.js
+++ src/CommonComponents/Resource/index.style.js
@@ -5,6 +5,8 @@ export const Container = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
   border-radius: 2.5rem;
+  /* padding: 3rem 1rem 3rem 1rem;
+  margin: 3rem 3rem 2rem 3rem; */
 `;
 
 export const CardItem = styled.a`
@@ -20,9 +22,33 @@ export const CardItem = styled.a`
 export const Image = styled.img`
   margin: 2rem 8rem 0 8rem;
   height: 23.5rem;
-  border-radius: 2.5rem;
+  border-radius: 0.5rem;
 `;
 export const Title = styled.span`
-  padding: 13rem 6rem 0 2rem;
-  font-size: 3.5rem;
+  padding: 5rem 0 3rem 2rem;
+  font-size: 3rem;
+  color: #205c88;
+  font-weight: 700;
+`;
+
+export const Price = styled.span`
+  font-weight: 600;
+  font-size: 2.5rem;
+  margin: 1.3rem 0 2rem 2rem;
+`;
+export const Description = styled.p`
+  font-size: 2.1rem;
+  line-height: 5.5rem;
+  color: #5b5a5a;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  padding: 0rem 19rem 0 2rem;
+  margin: 8rem -14rem 1rem -25rem;
+`;
+
+export const TextOne = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
