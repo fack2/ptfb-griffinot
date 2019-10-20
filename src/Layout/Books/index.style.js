2e3a389ff06a327b2170e2deb55904d13c3fda28
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-20 09:27:54 +0300

    resolve the conflict

---
+++ src/Layout/Books/index.style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ImgTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 3rem 0 0 5rem;
+`;
+export const Imglogo = styled.img`
+  display: flex;
+  height: 10rem;
+  width: 10rem;
+`;
+export const TitlePage = styled.span`
+  padding: 1rem 0 0 4rem;
+  font-size: 5.5rem;
+  color: #205c8b;
+  font-weight: 600;
+`;
