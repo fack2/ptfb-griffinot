73a935958262eb8beee186cbd2ec2171338339cc
95Rawan <youlostme17@hotmail.com>
2019-10-21 16:27:27 +0300

    Passed nextButtonClickHandler to it and changed the paragraph to a button so we can put a click handler on it

--- src/Layout/Questionnaire/Option3.js
+++ src/Layout/Questionnaire/Option3.js
@@ -1,38 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
-  TitleOption, OptionDiv, ParagraphObtion, DivData, LinkOption,
+  TitleOption, Option, DivData, LinkOption,
 } from './index.style';
 import PrevButton from '../../CommonComponents/PrevButton';
 
-const Option3 = () => (
+const Option3 = ({ nextButtonClickHandler }) => (
   <>
     <TitleOption>
       What skill do you feel your child needs the most help with improving, at the moment?
     </TitleOption>
 
     <DivData>
-      <LinkOption href="/">
-        <OptionDiv>
-          <ParagraphObtion>I would like to help them to improve their pencil grasp</ParagraphObtion>
-        </OptionDiv>
+      <LinkOption to="/questionnaire">
+        <Option>I would like to help them to improve their pencil grasp</Option>
       </LinkOption>
-      <LinkOption href="/">
-        <OptionDiv>
-          <ParagraphObtion>
-            I would like to help them to improve their general fine motor skills.
-          </ParagraphObtion>
-        </OptionDiv>
+      <LinkOption to="/questionnaire">
+        <Option name="nextBtn" onClick={nextButtonClickHandler}>
+          I would like to help them to improve their general fine motor skills.
+        </Option>
       </LinkOption>
-      <LinkOption href="/">
-        <OptionDiv>
-          <ParagraphObtion>
+      <LinkOption to="/questionnaire">
+        <Option>
             I would like to help them to improve their scissor skills..
-          </ParagraphObtion>
-        </OptionDiv>
+        </Option>
       </LinkOption>
     </DivData>
 
-    <PrevButton prevLink="/questionnaire" />
+    <PrevButton prevLink="/questionnaire" nextButtonClickHandler={nextButtonClickHandler} />
   </>
 );
+
+Option3.propTypes = {
+  nextButtonClickHandler: PropTypes.func.isRequired,
+};
 export default Option3;
