2dffea3d8f9f6b45d39152fc874f986a682b9943
sara219 <whitenoise6014@gmail.com>
2019-10-21 10:13:56 +0300

    Added an option text the go to option link

--- src/CommonComponents/PopupPage/index.js
+++ src/CommonComponents/PopupPage/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
 import PropTypes from 'prop-types';
 import popUpImg from '../../assets/popup-logo.png';
@@ -11,31 +12,38 @@ import {
   ContainerBtn,
   Container2,
   TextBtn,
+  LinkText
 } from './index.style';
 
-const PopupPage = ({ description, NextLink }) => (
-  <>
-    <BodyPage>
-      <PopUPCard>
-        <PopUpBox Height="80rem">
-          <Logo>
-            <LogoImage src={popUpImg} alt="a kid holding a kite logo" />
-          </Logo>
-          <Container2>
-            <PopUpDiv>{description}</PopUpDiv>
-            <ContainerBtn>
-              <TextBtn to={NextLink}>I understand</TextBtn>
-            </ContainerBtn>
-          </Container2>
-        </PopUpBox>
-      </PopUPCard>
-    </BodyPage>
-  </>
-);
+const PopupPage = ({
+  description, optionText, optionLink, NextLink,
+}) => (
+    <>
+      <BodyPage>
+        <PopUPCard>
+          <PopUpBox Height="80rem">
+            <Logo>
+              <LogoImage src={popUpImg} alt="a kid holding a kite logo" />
+            </Logo>
+            <Container2>
+              <PopUpDiv>{description}</PopUpDiv>
+              <LinkText to={optionLink}>{optionText}</LinkText>
+              <ContainerBtn>
+                <TextBtn to={NextLink}>I understand</TextBtn>
+              </ContainerBtn>
+            </Container2>
+          </PopUpBox>
+        </PopUPCard>
+      </BodyPage>
+    </>
+  );
 
 PopupPage.propTypes = {
   description: PropTypes.string.isRequired,
   NextLink: PropTypes.element.isRequired,
+  // eslint-disable-next-line react/require-default-props
+  optionLink: PropTypes.element,
+  optionText: PropTypes.string,
 };
 
 export default PopupPage;
