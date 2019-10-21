503310fc7aeebfd96531ef284c62bd5ee12c0093
sara219 <whitenoise6014@gmail.com>
2019-10-21 21:22:40 +0300

    Create a new common component that include the pop-up fram and the level

---
+++ src/CommonComponents/LevelPop/index.js
@@ -0,0 +1,58 @@
+/* eslint-disable react/require-default-props */
+import React from 'react';
+import PropTypes from 'prop-types';
+import popUpImg from '../../assets/popup-logo.png';
+import {
+  PopUPCard,
+  PopUpBox,
+  Logo,
+  LogoImage,
+  BodyPage,
+  PopUpDiv,
+  Container2,
+  TextBtn,
+  CircleLevel,
+  ParagraphLevel,
+  TitleLevel,
+  LevelDiv,
+  TitleParagraph,
+} from './index.style';
+
+const PopupPage = ({
+  description, NextLink, levelScore, titleLevel, button,
+}) => (
+    <>
+      <BodyPage>
+        <PopUPCard>
+          <PopUpBox Height="100rem">
+            <Logo>
+              <LogoImage src={popUpImg} alt="a kid holding a kite logo" />
+            </Logo>
+            <Container2>
+              <LevelDiv>
+                <CircleLevel>
+                  <ParagraphLevel>{levelScore}</ParagraphLevel>
+                </CircleLevel>
+
+                <TitleLevel>
+                  <TitleParagraph>{titleLevel}</TitleParagraph>
+                </TitleLevel>
+              </LevelDiv>
+              <PopUpDiv FontSize="2rem">{description}</PopUpDiv>
+              <TextBtn to={NextLink}>{button}</TextBtn>
+            </Container2>
+          </PopUpBox>
+        </PopUPCard>
+      </BodyPage>
+    </>
+  );
+
+PopupPage.propTypes = {
+  description: PropTypes.string.isRequired,
+  NextLink: PropTypes.element.isRequired,
+  levelScore: PropTypes.string.isRequired,
+  titleLevel: PropTypes.string.isRequired,
+  button: PropTypes.string.isRequired,
+};
+
+export default PopupPage;
---
+++ src/CommonComponents/LevelPop/index.style.js
@@ -0,0 +1,126 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const LevelDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin-top: 5rem;
+  margin-bottom: 4.9rem;
+`;
+
+export const CircleLevel = styled.div`
+  @media (min-width: 1281px) {
+    width: 12rem;
+    height: 12rem;
+    background: #ffce1c;
+    border-radius: 11rem;
+    z-index: 1;
+  }
+    width: 15rem;
+    height: 15rem;
+  background: #ffce1c;
+  border-radius: 11rem;
+  z-index: 1;
+`;
+
+export const ParagraphLevel = styled.p`
+  @media (min-width: 1281px) {
+    font-size: 2.5rem;
+    margin-top: 4rem;
+    font-weight: bolder;
+    color: #e8425c;
+  }
+  font-size: 3.3rem;
+  margin-top: 5.6rem;   
+  font-weight: bolder;
+  color: #e8425c;
+`;
+
+export const TitleLevel = styled.div`
+  @media (min-width: 1281px) {
+    width: 45rem;
+    height: 8rem;
+    background: #205c8b;
+    border: 1px solid #a0a0a0;
+    box-sizing: border-box;
+    border-radius: 82px;
+    margin-top: 2rem;
+    margin-left: -11rem;
+  }
+  width: 68rem;
+  height: 10.9rem;
+  background: #205c8b;
+  border: 1px solid #a0a0a0;
+  box-sizing: border-box;
+  border-radius: 82px;
+  margin-top: 2.5rem;
+  margin-left: -9rem;
+`;
+
+export const TitleParagraph = styled.p`
+  @media (min-width: 1281px) {
+    font-size: 2rem;
+    margin-left:7.9rem;
+    color: #ffffff;
+  }
+  font-size: 3.5rem;
+  margin-left: 0.9rem;
+  color: #ffffff;
+`;
+
+export const BodyPage = styled.div`
+  background: #cfcfcf;
+  justify-content: center;
+`;
+
+export const PopUPCard = styled.div`
+  text-align: center;
+  display: flex;
+  height: 0.6rem;
+`;
+
+export const PopUpBox = styled.div`
+  width: ${(props) => props.Width || '82rem'};
+  height: ${(props) => props.Height || '100rem'};
+  background: #fffcfc;
+  border: 0.1rem solid #c4c4c4;
+  margin-left: -17.4rem;
+  margin-top: -38rem;`;
+
+export const Logo = styled.div`
+  width: 82.5rem;
+  height: 34rem;
+  background: #205c8b;
+  margin-top: -1rem;
+  margin-left: -0.4rem;
+`;
+
+export const LogoImage = styled.img`
+  width: 45rem;
+  height: 41rem;
+`;
+
+export const Container2 = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PopUpDiv = styled.span`
+  font-size: 2.7rem;
+  text-align: left;
+  margin-left: 5rem;
+  margin-top: 3rem;
+  letter-spacing: 0.1rem;
+`;
+
+export const TextBtn = styled(Link)`
+  position:absolute;
+  top: 54rem;
+  left: 20rem;
+  font-size: 3.5rem;
+  font-size: 4rem;
+  font-weight: bold;
+  color: var(--main-title-color);
+  text-decoration: none;
+`;
