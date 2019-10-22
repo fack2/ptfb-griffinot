5d6d2d0d7b167c4b0e0ed2053ec151137119ba42
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-22 11:24:18 +0300

    added midea query and edit style for book page

--- src/Layout/Books/index.style.js
+++ src/Layout/Books/index.style.js
@@ -3,21 +3,34 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  @media (min-width: 1281px) {
+    margin: 0 0 0 30rem;
+  }
 `;
 
 export const ImgTitle = styled.div`
   display: flex;
   flex-direction: row;
   padding: 3rem 0 0 5rem;
+  @media (min-width: 1281px) {
+    margin: 0 0 0 20rem;
+  }
 `;
 export const Imglogo = styled.img`
   display: flex;
   height: 10rem;
-  width: 10rem;
+  @media (min-width: 1281px) {
+    height: 4.5rem;
+  }
 `;
 export const TitlePage = styled.span`
   padding: 1rem 0 0 4rem;
   font-size: 5.5rem;
   color: #205c8b;
   font-weight: 600;
+  @media (min-width: 1281px) {
+    font-size: 3rem;
+    padding: 0 0 1.2rem 1rem;
+    margin: 0.5rem 0 0 0;
+  }
 `;
