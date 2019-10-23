4659ff8956b159952cde6272472898bcaa480945
someyaaltous <someyaaltous1995@gmail.com>
2019-10-23 19:54:45 +0300

    changed style

--- src/Layout/ResourcesCategories/index.style.js
+++ src/Layout/ResourcesCategories/index.style.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  justify-content: center;
 `;
 
 export const BodyCategory = styled.div`
@@ -11,7 +10,6 @@ export const BodyCategory = styled.div`
 
 export const TitleCategory = styled.h1`
   margin-left: 5rem;
-  margin-bottom: 9rem;
 `;
 
 export const Link = styled.a`
