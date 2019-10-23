3fe020fa22419798e706bc47047a35cedabc4159
someyaaltous <someyaaltous1995@gmail.com>
2019-10-23 20:50:35 +0300

    added data to fine motor skills

---
+++ src/Data/FineMotorData.js
@@ -0,0 +1,19 @@
+const FineMotorData = [
+  {
+    Title: 'What does a good pencil grasp look like',
+    ImageCategory: 'https://i.imgur.com/sVtJ64N.png',
+    LinkCategory:
+      'https://www.griffinot.com/what-does-good-pencil-grasp-look-like/',
+  },
+  {
+    Title: 'To Pencil Grip or Not to Pencil Grip',
+    ImageCategory: 'https://i.imgur.com/RYIwIj5.png',
+    LinkCategory: 'https://www.griffinot.com/pencil-grip-reviews/',
+  },
+  {
+    Title: 'Crocodile Snap – song and animation to help children',
+    ImageCategory: 'https://i.imgur.com/sVtJ64N.png',
+    LinkCategory: 'https://www.youtube.com/watch?v=N6kPcQSSsEY',
+  },
+];
+export default FineMotorData;
