61bd59057aa32802e22031fd654e008ab7056b84
someyaaltous <someyaaltous1995@gmail.com>
2019-10-23 20:51:25 +0300

    added fine motor layout

---
+++ src/Layout/ResourcesCategories/FineMotor.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import FineMotorData from '../../Data/FineMotorData';
+import {
+ TitleCategory, Container, BodyCategory, Link 
+} from './index.style';
+import ResourcesCategory from '../../CommonComponents/ResourcesCategory';
+import PrevButton from '../../CommonComponents/PrevButton';
+
+const FineMotor = () => (
+  <>
+    <Container>
+      <TitleCategory>Fine Motor Skill Tips</TitleCategory>
+    </Container>
+    <BodyCategory>
+      {FineMotorData.map(({ Title, ImageCategory, LinkCategory }) => (
+        <Link href={LinkCategory}>
+          <ResourcesCategory Title={Title} ImageCategory={ImageCategory} />
+        </Link>
+      ))}
+    </BodyCategory>
+    <PrevButton prevLink="/resources" margin="29rem" />
+  </>
+);
+export default FineMotor;
