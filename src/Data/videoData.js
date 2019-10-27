b50cb1497d2523de97421067af26fe1fd578b4d1
95Rawan <youlostme17@hotmail.com>
2019-10-27 15:08:15 +0200

    Changed height and width to each video in the data file

--- src/Data/videoData.js
+++ src/Data/videoData.js
@@ -1,15 +1,15 @@
 const videoData = [
   {
     id: 1,
-    height: '45rem',
-    width: '80rem',
+    width: '355px',
+    height: '230px',
     title: 'Introduction to FMSP GriffinOT',
     video: 'https://www.youtube.com/embed/2Ma_nnbJsYE',
   },
   {
     id: 2,
-    height: '45rem',
-    width: '80rem',
+    width: '355px',
+    height: '230px',
     title: 'How to Run the Programme:',
     video: 'https://www.youtube.com/embed/h_zZzdhdFCo',
   },
