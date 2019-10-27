4c564991eaee684b6728170a3f30ab297f91931a
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-27 06:21:10 +0200

    Appearing the Header in the all page expext Menue & Not page found add an array it's name Paths, andstore in this array all of the pathes i need the Header appearing at it. add condition for check if the path was orderd from the user is store in the array or not

--- src/CommonComponents/Header/index.js
+++ src/CommonComponents/Header/index.js
@@ -3,17 +3,32 @@ import { NavBar, Logo, Menu } from './index.style';
 import logo from '../../assets/logo.png';
 import menu from '../../assets/menu.png';
 
-const Header = (props) => (
-  <>
-    <NavBar>
-      <a href="/">
-        <Logo alt="logo" src={logo} />
-      </a>
-      <a href="/menu">
-        <Menu alt="menu" src={menu} {...props} />
-      </a>
-    </NavBar>
-  </>
-);
+const Header = (props) => {
+  const {
+    location: { pathname },
+  } = props;
+  const paths = [
+    '/',
+    '/levels',
+    '/resources',
+    '/questionnaire',
+    '/schema',
+    '/about-us',
+    '/introductory-videos',
+    '/books',
+  ];
+  return paths.includes(pathname) ? (
+    <>
+      <NavBar>
+        <a href="/">
+          <Logo alt="logo" src={logo} />
+        </a>
+        <a href="/menu">
+          <Menu alt="menu" src={menu} {...props} />
+        </a>
+      </NavBar>
+    </>
+  ) : null;
+};
 
 export default Header;
