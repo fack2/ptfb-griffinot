19de9a09fe781e809c40df495f7a39575ea669ee
95Rawan <youlostme17@hotmail.com>
2019-10-27 14:41:28 +0200

    Changed style of resource component to be in percentage instead of rem

--- src/CommonComponents/Resource/index.style.js
+++ src/CommonComponents/Resource/index.style.js
@@ -1,53 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 4rem;
-  align-self: center;
-`;
-export const CardItem = styled.a`
-  width: 86rem;
-  height: 14rem;
+export const ResourseCard = styled.a`
   border: 0.2rem solid #a7a7a7;
   border-radius: 1rem;
-  margin-top: 19rem;
   display: flex;
-  width: 85rem;
-  height: 34rem;
+  width: 90%;
   align-items: center;
-  @media screen and (min-width: 1024px) and (max-width: 1280px) {
-    margin: 3rem 7rem 0rem 7rem;
-    width: 75rem;
-    height: 27rem;
-  }
-
-  @media screen and (min-width: 1281px) {
-    margin: 3rem 30rem 0rem 30rem;
-  }
+  flex-direction: row;
+  margin-top: 7%;
 `;
 export const Image = styled.img`
-  height: 23.5rem;
+  width: 30%;
+  margin: 3% 10% 3% 4%;
   border-radius: 0.5rem;
-  margin-left: 7rem;
-  @media screen and (min-width: 1024px) and (max-width: 1280px) {
-    margin: 0 8rem;
-    height: 19rem;
-  }
 `;
 export const Title = styled.span`
-  font-size: 3.3rem;
   color: #205c88;
+  font-size: 1.7rem;
   font-weight: 700;
-  margin-left: 11rem;
-  margin: ${(props) => props.margin || '10rem -7rem 1rem - 7rem'};
-  align-self: center;
-  @media screen and (min-width: 1024px) and (max-width: 1280px) {
-    margin: 4rem 0 0 4.5rem;
-    font-size: 2.4rem;
-  }
-  @media screen and (min-width: 1280px){
-    margin: 4rem 0 0   15rem;
-    font-size: 2.4rem;
-  }
 `;
