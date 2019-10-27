790ba3d77a0213c9151dff4b10331a7b5eaf555f
95Rawan <youlostme17@hotmail.com>
2019-10-27 14:48:41 +0200

    Changed styled components names and style to percentages instead of rem

--- src/Layout/Resources/index.style.js
+++ src/Layout/Resources/index.style.js
@@ -1,18 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-export const TitleH = styled.span`
-  padding: 0 0 0 5rem;
-  font-size: 4.5rem;
+export const PageTitle = styled.span`
+  font-size: 1.7rem;
   font-weight: 600;
   color: #205c88;
-  display: block;
-  margin: 8rem 0rem 8rem 1rem;
-  @media screen and (min-width: 1024px) and (max-height: 1280px) {
-    margin: 10rem 0 2rem 8rem;
-    font-size: 3rem;
-  }
+  margin: 0% 0% 2% 5%;
+`;
+
+export const ResoursesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
