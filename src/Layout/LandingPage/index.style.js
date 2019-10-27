07f30d8f05f28a6157711cb665f8508beef70fe8
sara219 <whitenoise6014@gmail.com>
2019-10-27 10:45:15 +0200

    Edite the style and change some of it

--- src/Layout/LandingPage/index.style.js
+++ src/Layout/LandingPage/index.style.js
@@ -2,77 +2,51 @@ import styled from 'styled-components';
 import '../../index.css';
 
 export const Container = styled.div`
-  display: flex;
+  /* display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: space-around; */
 `;
 
 export const Title = styled.h1`
-  font-size: 4.5rem;
+  font-size: 1.8rem;
   text-align: center;
-  margin-bottom: 5rem;
+  margin-top: 15%;
+  margin-bottom: 3%;
 `;
 
 export const LandingImg = styled.img`
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 2rem;
-  width: 84rem;
-  border-radius: 1.5rem;
-  @media (min-width: 1281px) {
-    border-radius: 0.8rem;
-    width: 70rem;
-  }
+  width: 85%;
+  border-radius: 2%;
+  margin-left: 7%;
+  margin-right: 7%;
+  margin-top: 3%;
+
 `;
 
 export const Paragraph = styled.p`
-  font-size: 1.7rem;
-  color: #5c5b5b;
-  margin: 8rem 7rem 0rem 7rem;
-  @media (min-width: 1024px) and (max-width: 1280px) {
-    font-size: 2.5rem;
-    margin: 8rem 17rem 0rem 21rem;
-  }
-  @media (min-width: 1281px) {
-    font-size: 2.5rem;
-    margin: 8rem 47rem 0rem 47rem;
-  }
+  font-size: 1.5rem;
+  color: #3a3a3a;
+  margin-left: 8%;
+  margin-right: 8%;
+  text-align: justify;
 `;
 
 export const Button = styled.button`
-  margin-left: auto;
-  margin-right: auto;
   background-color: var(--main-title-color);
   border: 1px solid #205c8b;
   color: white;
   border-radius: 20rem;
-  width: 61rem;
-  height: 11rem;
-  margin-top: 10rem;
-  font-size: 4.8rem;
+  font-size: 1.9rem;
   font-weight: bolder;
-  @media (min-width: 1024px) and (max-width: 1280px) {
-    width: 45rem;
-    height: 9rem;
-    font-size: 3.5rem;
-  }
-  @media (min-width: 1281px) {
-    width: 45rem;
-    height: 9rem;
-    font-size: 3.5rem;
-  }
+  width: 55%;
+  height: 45px;
+  margin-left: 22%;
+  margin-top: 5%;
 `;
 
 export const ResourceLink = styled.p`
-  font-size: 3rem;
-  margin-left: auto;
-  margin-right: auto;
-  @media (min-width: 1024px) and (max-width: 1280px) {
-    font-size: 2.4rem;
-  }
-  @media (min-width: 1281px) {
-    font-size: 2rem;
-  }
+  font-size: 1.5rem;
+  margin-left: 24%;
 `;
 
 export const Link = styled.span`
