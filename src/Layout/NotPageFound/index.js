fb044197212d1ab1a3f12d7a9c240ba02380ec74
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-27 10:07:27 +0200

    delete this tag <></>

--- src/Layout/NotPageFound/index.js
+++ src/Layout/NotPageFound/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import NotFoundPageImg from '../../assets/NotPageFound.png';
-import {
- Container, NotFound, Description, NotFoundImg 
-} from './index.style';
+import NotFoundPageImg from '../../assets/NotFound.png';
+import { Container, NotFound, Description, NotFoundImg } from './index.style';
 
 const NotPageFound = () => (
-  <>
-    <Container>
-      <NotFound>Page Not Found</NotFound>
-      <NotFoundImg src={NotFoundPageImg} />
-      <Description>
-        Sorry, we couldn't find the page you were for looking for.
-        <br />
-        We suggist that you return to main section.
-      </Description>
-    </Container>
-  </>
+  <Container>
+    <NotFoundImg src={NotFoundPageImg} />
+    <Description>
+      Sorry, we couldn't find the page you were for looking for.
+      <br />
+      We suggist that you return to main page.
+    </Description>
+  </Container>
 );
 export default NotPageFound;
