bef9efe07762e4eb5b8f19b18ac2609ee6517234
95Rawan <youlostme17@hotmail.com>
2019-10-27 10:07:47 +0200

    Changed the style of the header after adding the meta viewport

--- src/CommonComponents/Header/index.style.js
+++ src/CommonComponents/Header/index.style.js
@@ -2,23 +2,34 @@ import styled from 'styled-components';
 
 export const NavBar = styled.div`
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10%;
+
+  /* margin: 3% -13% 7% 2%; */
+
+  /* display: flex;
   justify-content: center;
   justify-content: space-between;
   @media screen and (min-width: 1281px) {
     justify-content: space-around;
-  }
+  } */
 `;
 
 export const Logo = styled.img`
-  width: 35rem;
+  width: 60%;
+  /* width: 35rem;
   @media screen and (min-width: 1024px) and (max-width: 1280px) {
     align-self: flex-start;
     margin-left: 3rem;
-  }
+  } */
 `;
 
 export const Menu = styled.img`
-  width: 6rem;
+  width: 35%;
+  margin-left: 48%;
+
+  /* width: 6rem;
   margin: 7rem 4rem 0rem 46rem;
   @media screen and (min-width: 1024px) and (max-width: 1280px) {
     align-self: flex-end;
@@ -26,5 +37,5 @@ export const Menu = styled.img`
   }
   @media screen and (min-width: 1281px) {
     margin: 7rem 10rem 0rem 26rem;
-  }
+  } */
 `;
