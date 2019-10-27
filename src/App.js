f465c246b83f8bd1a99f50273d488e908574834b
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-27 06:12:42 +0200

    call the component Not page found in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './CommonComponents/Header';
 import LandingPage from './Layout/LandingPage';
 import Menu from './Layout/Menu';
@@ -10,21 +10,25 @@ import Resources from './Layout/Resources';
 import Questionnaire from './Layout/Questionnaire';
 import Books from './Layout/Books';
 import Videos from './Layout/Videos';
+import NotPageFound from './Layout/NotPageFound';
 
 function App() {
   return (
     <>
       <Router>
-        <Route render={() => (window.location.pathname === '/menu' ? null : <Header />)} />
-        <Route exact path="/menu" component={Menu} />
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/schema" component={Schema} />
-        <Route exact path="/about-us" component={AboutUs} />
-        <Route exact path="/levels" component={Levels} />
-        <Route exact path="/resources" component={Resources} />
-        <Route exact path="/questionnaire" component={Questionnaire} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/introductory-videos" component={Videos} />
+        <Route component={Header} />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/menu" component={Menu} />
+          <Route exact path="/schema" component={Schema} />
+          <Route exact path="/about-us" component={AboutUs} />
+          <Route exact path="/levels" component={Levels} />
+          <Route exact path="/resources" component={Resources} />
+          <Route exact path="/questionnaire" component={Questionnaire} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/introductory-videos" component={Videos} />
+          <Route component={NotPageFound} />
+        </Switch>
       </Router>
     </>
   );
