3ae2262285514e69176a7c04c9d3ca527856041d
someyaaltous <someyaaltous1995@gmail.com>
2019-10-28 12:21:24 +0200

    deleted comment

--- src/Layout/Levels/index.style.js
+++ src/Layout/Levels/index.style.js
@@ -11,57 +11,9 @@ export const Title = styled.h1`
   margin-left: 3%;
   font-weight: bold;
   margin-bottom: 7%;
-
 `;
-
 
 export const Link = styled.a`
   color: #2d2323;
   text-decoration: none;
 `;
-
-
-/* Media Query for Tablets Ipads portrait mode */
-// @media (min-width: 768px) and (max-width: 1024px) {
-//   body {
-//     width: 80%;
-//     margin: 0 auto;
-//   }
-//   h1 {
-//     font-size: 2.5rem;
-//   }
-// }
-
-// /* Media Query for Laptops and Desktops */
-// @media (min-width: 1025px) and (max-width: 1280px) {
-//   body {
-//     width: 60%;
-//     margin: 0 auto;
-//   }
-//   h1 {
-//     font-size: 3rem;
-//   }
-// }
-
-// /* Media Query for Large screens */
-// @media (min-width: 1281px) {
-//   body {
-//     width: 60%;
-//     margin: 0 auto;
-//   }
-//   h1 {
-//     font-size: 3.5rem;
-//   }
-// }
-
-// /* Media Query for Large screens */
-// @media (min-width: 1281px) {
-//   body {
-//     width: 60%;
-//     margin: 0 auto;
-//   }
-//   h1 {
-//     font-size: 3.5rem;
-//   }
-
-// }
