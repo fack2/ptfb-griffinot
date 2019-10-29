a53df32a0e04dec06741d828fb2ee89e187b9743
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-29 10:59:53 +0200

    make a new style for Quastionnaire pages

--- src/Layout/Questionnaire/index.style.js
+++ src/Layout/Questionnaire/index.style.js
@@ -3,29 +3,45 @@ import { Link } from 'react-router-dom';
 
 export const Header = styled.h1`
   text-align: center;
-  margin: 8rem 4rem 4rem 4rem;
+  margin: 0% 0% 3.5% 0;
+  font-size: 1.5rem;
 
-  @media screen and (min-width: 1024px) and (max-width: 1280px) {
+  /* Media Query for Mobile Devices */
+  /* @media (max-width: 480px) {
+    
+  } */
+  /* @media screen and (min-width: 1024px) and (max-width: 1280px) {
     font-size: 2.5rem;
     margin: 7rem 4rem 4rem 4rem;
   }
   @media screen and (min-width: 1281px) {
     font-size: 2.7rem;
     margin: 5rem 8rem 5rem 3rem;
-  }
+  } */
 `;
 
 export const Line = styled.hr`
   border-top: 1px solid var(--main-font-color);
-  margin: 0rem 12rem;
+  margin: 0% 0%;
+  /* Media Query for Mobile Devices */
+  /* @media (max-width: 480px) {
+    
+  } */
 
-  @media screen and (min-width: 1281px) {
+  /* @media screen and (min-width: 1281px) {
     margin: 1rem 24rem 1rem 19rem;
-  }
+  } */
 `;
 
 export const Paragraph = styled.p`
-  font-size: 3.5rem;
+  font-size: 1.5rem;
+  margin: 4% 0 3% 0%;
+  font-weight: bold;
+  /* Media Query for Mobile Devices */
+  /* @media (max-width: 480px) {
+
+  } */
+  /* font-size: 3.5rem;
   margin: 8rem 2rem 2rem 3rem;
   font-weight: bold;
   @media screen and (min-width: 1024px) and (max-width: 1280px) {
@@ -35,11 +51,11 @@ export const Paragraph = styled.p`
   @media screen and (min-width: 1281px) {
     font-size: 2.3rem;
     margin: 4rem 0 3rem 20rem;
-  }
+  } */
 `;
 
 export const DivData = styled.div`
-  display: flex;
+  /* display: flex;
   margin-left: 6rem;
   margin-top: 11rem;
   flex-direction: column;
@@ -47,20 +63,20 @@ export const DivData = styled.div`
   @media (min-width: 1000px) {
     display: -webkit-flex;
     text-align: -webkit-center;
-  }
+  } */
 `;
 
 export const TitleOption = styled.h1`
-  @media (min-width: 1000px) {
+  /* @media (min-width: 1000px) {
     font-size: 2.4rem;
     text-align: center;
   }
   font-size: 3.8rem;
-  text-align: center;
+  text-align: center; */
 `;
 
 export const Option = styled.button`
-  @media (min-width: 1000px) {
+  /* @media (min-width: 1000px) {
     width: 50rem;
     height: 15rem;
     background: #d6ebf7;
@@ -81,29 +97,32 @@ export const Option = styled.button`
   font-size: 3.7rem;
   color: #000000;
   text-align: center;
-  outline: none;
+  outline: none; */
 `;
 
 export const LinkOption = styled(Link)`
-  text-decoration: none;
+  /* text-decoration: none; */
 `;
 
 export const QuestionContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 3rem;
+  margin: 3% -3% 0 4%;
+  justify-content: center;
 `;
 
 export const QuestionCard = styled.div`
-  border-radius: 2rem;
+  border-radius: 0.8rem;
   border: 0.5px solid #928e8e;
   box-sizing: border-box;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  height: ${(props) => props.height || '18rem'};
-  padding: 4rem 3rem;
-  margin: 4rem 2rem 0;
+  height: ${(props) => props.height || '78px'};
+  padding: 5% 2% 0 2%;
+  margin: 2.5% 2% 3.5% -5%;
   display: flex;
   flex-wrap: wrap;
+
+  /* border-radius: 2rem;
   @media screen and (min-width: 1024px) and (max-width: 1280px) {
     width: 75rem;
     margin: 3rem 0rem 1rem 13rem;
@@ -112,20 +131,22 @@ export const QuestionCard = styled.div`
     width: 83rem;
     height: 18rem;
     margin: 3rem 0rem 1rem 20rem;
-  }
+  } */
 `;
 
 export const QuestionNumber = styled.span`
   font-weight: bold;
-  width: 4.3rem;
-  height: 3.9rem;
-  padding-top: 0.5rem;
-  font-size: 2.5rem;
+  width: 18px;
+  height: 18px;
+  padding-top: 0.5%;
+  line-height: 150%;
+  font-size: 1rem;
   color: #ffffff;
   background: var(--next-prev-button-color);
   border-radius: 50%;
   text-align: center;
-  @media screen and (min-width: 1024px) and (max-width: 1280px) {
+  padding: 0.5% 0.5% 0 0;
+  /*  @media screen and (min-width: 1024px) and (max-width: 1280px) {
     height: 3rem;
     width: 3rem;
     line-height: 91%;
@@ -137,16 +158,16 @@ export const QuestionNumber = styled.span`
     line-height: 68%;
     font-size: 1.9rem;
     margin-top: 1rem;
-  }
+  } */
 `;
 
 export const QuestionText = styled.p`
-  margin: -0.4rem 0 0 2rem;
+  margin: -0.5% 0 0 3%;
   font-weight: 600;
-  font-size: 3.8rem;
+  font-size: 1.5rem;
   color: var(--main-font-color);
   flex-basis: 85%;
-  @media screen and (min-width: 1024px) and (max-width: 1280px) {
+  /* @media screen and (min-width: 1024px) and (max-width: 1280px) {
     font-size: 3rem;
   }
   @media screen and (min-width: 1281px) {
@@ -154,30 +175,30 @@ export const QuestionText = styled.p`
     font-weight: 400;
     margin: -0.4rem 0 0 2rem;
     padding: 0.9rem 0 0 0;
-  }
+  } */
 `;
 
 export const Checkbox = styled.input`
-  width: 3.8rem;
-  height: 3.8rem;
+  width: 20px;
+  height: 20px;
   background: #ffffff;
   border: 1px solid #787878;
   box-sizing: border-box;
-  margin-left: 1.5rem;
-  @media screen and (min-width: 1024px) and (max-width: 1280px) {
+  margin: 0 -5% 0 1.5%;
+  /*@media screen and (min-width: 1024px) and (max-width: 1280px) {
     width: 2.5rem;
   }
   @media screen and (min-width: 1281px) {
     width: 2.5rem;
-  }
+  } */
 `;
 
 export const QuestionDescription = styled.p`
-  margin: 0.8rem 4rem 0.8rem 6rem;
+  margin: ${(props) => props.margin || '0%'};
   font-weight: 600;
-  font-size: 3rem;
+  font-size: 1.3rem;
   color: #2d2323;
-  @media screen and (min-width: 1024px) and (max-width: 1280px) {
+  /*@media screen and (min-width: 1024px) and (max-width: 1280px) {
     font-size: 1.8rem;
     margin: 0 0 2rem 2rem;
     padding: 0 2rem 0 0;
@@ -186,31 +207,32 @@ export const QuestionDescription = styled.p`
     font-size: 1.9rem;
     font-weight: 400;
     margin: 0 8rem 3rem 2rem;
-  }
+  } */
 `;
+
 export const DivNextPrev = styled.div`
-  display: flex;
-  justify-content: space-around;
+  /* display: flex;
+  justify-content: space-around; */
 `;
 
 export const LevelDiv = styled.div`
-  display: flex;
+  /* display: flex;
   justify-content: center;
   margin-top: 5rem;
-  margin-bottom: 4.9rem;
+  margin-bottom: 4.9rem; */
 `;
 
 export const CircleLevel = styled.div`
-  width: 30rem;
+  /* width: 30rem;
   height: 30rem;
   border: 0.8rem #8ac456 solid;
   border-radius: 54rem;
   z-index: 1;
-  margin-top: -4rem;
+  margin-top: -4rem; */
 `;
 
 export const ParagraphLevel = styled.p`
-  @media (min-width: 1281px) {
+  /* @media (min-width: 1281px) {
     font-size: 2.5rem;
     margin-top: 4rem;
     font-weight: bolder;
@@ -219,83 +241,83 @@ export const ParagraphLevel = styled.p`
   font-size: 3.3rem;
   margin-top: 5.6rem;
   font-weight: bolder;
-  color: #e8425c;
+  color: #e8425c; */
 `;
 
 export const LevelIntro = styled.p`
-  font-size: 3rem;
+  /* font-size: 3rem;
   font-weight: bold;
   margin-left: -9rem;
-  margin-top: 6rem;
+  margin-top: 6rem; */
 `;
 
 export const TitleParagraph = styled.p`
-  @media (min-width: 1281px) {
+  /* @media (min-width: 1281px) {
     font-size: 2rem;
     margin-left: 7.9rem;
     color: #ffffff;
   }
   font-size: 3.5rem;
   margin-left: 0.9rem;
-  color: #ffffff;
+  color: #ffffff; */
 `;
 
 export const BodyPage = styled.div`
-  background: #cfcfcf;
-  justify-content: center;
+  /* background: #cfcfcf;
+  justify-content: center; */
 `;
 
 export const PopUPCard = styled.div`
-  text-align: center;
+  /* text-align: center;
   display: flex;
-  height: 0.6rem;
+  height: 0.6rem; */
 `;
 
 export const PopUpBox = styled.div`
-  width: 82rem;
+  /* width: 82rem;
   height: 129rem;
   background: #fffcfc;
   border: 0.1rem solid #c4c4c4;
   margin-left: -17.4rem;
-  margin-top: -58rem;
+  margin-top: -58rem; */
 `;
 
 export const Logo = styled.div`
-  width: 82.5rem;
+  /* width: 82.5rem;
   height: 34rem;
   background: #205c8b;
   margin-top: -1rem;
-  margin-left: -0.4rem;
+  margin-left: -0.4rem; */
 `;
 
 export const LogoImage = styled.img`
-  width: 45rem;
-  height: 41rem;
+  /* width: 45rem;
+  height: 41rem; */
 `;
 
 export const Container2 = styled.div`
-  display: flex;
-  flex-direction: column;
+  /* display: flex;
+  flex-direction: column; */
 `;
 
 export const PopUpDiv = styled.span`
-  font-size: 2.7rem;
+  /* font-size: 2.7rem;
   text-align: left;
   margin-left: 5rem;
   margin-top: -2rem;
   margin-right: 2rem;
-  letter-spacing: 0.1rem;
+  letter-spacing: 0.1rem; */
 `;
 
 export const ToLink = styled.a`
-  color: var(--main-title-color);
+  /* color: var(--main-title-color);
   text-decoration: none;
   font-size: 3.9rem;
   font-weight: bold;
-  margin: 5rem 0rem 0rem 37rem;
+  margin: 5rem 0rem 0rem 37rem; */
 `;
 
 export const Para = styled.p`
-  font-size: 2.5rem;
-  margin-top: 12rem;
+  /* font-size: 2.5rem;
+  margin-top: 12rem; */
 `;
