38a4dcd119ff170953af41cddcffe5cc08174dd5
95Rawan <youlostme17@hotmail.com>
2019-10-29 14:16:30 +0200

    Changed logo and menu sizes using width instead of height and used view hieght instead of percentage

--- src/CommonComponents/Header/index.style.js
+++ src/CommonComponents/Header/index.style.js
@@ -6,31 +6,32 @@ export const NavBar = styled.div`
   flex-direction: row;
   align-items: center;
   margin-bottom: 10%;
+  justify-content: space-between;
 `;
 
 export const Logo = styled.img`
-  margin-left: -6%;
-  width: 65%;
+  margin-left: -12%;
+  height: 12vh;
+
   @media (min-width: 481px) and (max-width: 767px) {
-    width: 55%;
+    height: 15vh;
   }
 
   @media (min-width: 768px) and (max-width: 1280px) {
-    width: 50%;
+    height: 15vh;
   }
 `;
 
 export const Menu = styled.img`
-  width: 40%;
-  margin-left: 50%;
+  margin-left: 2%;
+  height: 5vh;
 
   @media (min-width: 481px) and (max-width: 767px) {
-    width: 28%;
+    height: 7vh;
   }
 
   @media (min-width: 768px) and (max-width: 1280px) {
-    width: 30%;
-    margin-left: 60%;
+    height: 7vh;
   }
 `;
 
