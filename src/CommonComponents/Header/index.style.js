a88874fb4bd90211a6a98548298cbfb5cdb618d3
95Rawan <youlostme17@hotmail.com>
2019-10-29 11:09:59 +0200

    Made header responsive

--- src/CommonComponents/Header/index.style.js
+++ src/CommonComponents/Header/index.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const NavBar = styled.div`
   display: flex;
@@ -8,10 +9,31 @@ export const NavBar = styled.div`
 `;
 
 export const Logo = styled.img`
-  width: 60%;
+  margin-left: -6%;
+  width: 65%;
+  @media (min-width: 481px) and (max-width: 767px) {
+    width: 55%;
+  }
+
+  @media (min-width: 768px) and (max-width: 1280px) {
+    width: 50%;
+  }
 `;
 
 export const Menu = styled.img`
-  width: 35%;
-  margin-left: 48%;
+  width: 40%;
+  margin-left: 50%;
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    width: 28%;
+  }
+
+  @media (min-width: 768px) and (max-width: 1280px) {
+    width: 30%;
+    margin-left: 60%;
+  }
+`;
+
+export const LogoMenuLink = styled(Link)`
+  display: flex;
 `;
