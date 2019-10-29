f09f3a0720083020bb0a3a0d03befc41fd195178
95Rawan <youlostme17@hotmail.com>
2019-10-29 10:48:22 +0200

    Made the books page responsive

--- src/Layout/Books/index.style.js
+++ src/Layout/Books/index.style.js
@@ -4,24 +4,34 @@ export const BooksLogoTitleContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin-left: 4%;
+  margin-left: 3%;
+
+  @media (min-width: 481px) and (max-width: 767px) {
+    margin-left: 6%;
+  }
 `;
 
 export const PageLogo = styled.img`
-  width: 10%;
+  width: 8%;
+  @media (min-width: 481px) and (max-width: 767px) {
+    width: 8.5%;
+  }
 `;
 
-export const PageTitle = styled.span`
-  font-size: 2rem;
+export const PageTitle = styled.h1`
   color: #205c8b;
   font-weight: 600;
-  margin-left: 7%;
+  margin-left: 4%;
+  @media (min-width: 768px) and (max-width: 1024px) {
+    font-size: 3.3rem;
+  }
+  @media (min-width: 1025px) and (max-width: 1280px) {
+  }
 `;
 
 export const BooksContainer = styled.div`
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
+  flex-direction: column;
   margin-top: 7%;
+  align-items: center;
 `;
