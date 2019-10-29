f3b9628c78b80ae729144c0ae01bd51a4e5f66f8
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-29 22:03:53 +0200

    make a style more responsive make responsive for all devices that have a width size between 768px to 1024pxadd an appropriate props margin and height in this file for each question make responsive for all devices that have a width size between 1025px to 1280px and add an appropriate margin and height props in this file for each question

--- src/Layout/Questionnaire/SectionA.js
+++ src/Layout/Questionnaire/SectionA.js
@@ -1,11 +1,11 @@
 /* eslint-disable react/jsx-indent */
-import React from "react";
-import PropTypes from "prop-types";
-import Popup from "reactjs-popup";
-import LevelData from "./QuestionnaireData";
-import PopupPage from "../../CommonComponents/PopupPage";
-import data from "../../Data/questionnareData";
-import QuestionnaireData from "./QuestionnaireData";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Popup from 'reactjs-popup';
+import LevelData from './QuestionnaireData';
+import PopupPage from '../../CommonComponents/PopupPage';
+import data from '../../Data/questionnareData';
+import QuestionnaireData from './QuestionnaireData';
 import {
   QuestionCard,
   QuestionText,
@@ -15,15 +15,15 @@ import {
   QuestionContainer,
   Header,
   Paragraph,
-  Line
-} from "./index.style";
-import NextButton from "../../CommonComponents/NextButton";
+  Line,
+} from './index.style';
+import NextButton from '../../CommonComponents/NextButton';
 
 const SectionA = ({
   checkboxChange,
   nextButtonClickHandler,
   checkedQuestions,
-  checkedItems
+  checkedItems,
 }) => (
   <>
     <Header>
@@ -34,33 +34,41 @@ const SectionA = ({
       <Paragraph>
         Please check the box if your child is able to do the following:
       </Paragraph>
-      <QuestionCard height='110px'>
+      <QuestionCard height="110px" height3="95px" height4="90x">
         <QuestionNumber>{data[0].questionNumber}</QuestionNumber>
         <QuestionText>{data[0].question}</QuestionText>
         <Checkbox
-          type='checkbox'
+          type="checkbox"
           id={data[0].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[0].questionNumber - 1]}
         />
         <QuestionDescription
-          margin1='-4.5% 6% 0% 10%'
-          margin2='-6.5% 10% 0% 8%'
+          margin1="-4.5% 6% 0% 10%"
+          margin2="-6.5% 10% 0% 8%"
+          margin3="-1.5% 6% 0% 7.5%"
+          margin4="-1.8% 4% 0 7%"
         >
           {data[0].description}
         </QuestionDescription>
       </QuestionCard>
 
-      <QuestionCard height='141px'>
+      <QuestionCard height="141px" height3="125px" height4="90x">
         <QuestionNumber>{data[1].questionNumber}</QuestionNumber>
         <QuestionText>{data[1].question}</QuestionText>
         <Checkbox
-          type='checkbox'
+          type="checkbox"
           id={data[1].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[1].questionNumber - 1]}
         />
-        <QuestionDescription margin1='-3% 7% 0% 10%' margin2='-6.5% 8% 0% 8%'>
+
+        <QuestionDescription
+          margin1="-3% 7% 0% 10%"
+          margin2="-6.5% 8% 0% 8%"
+          margin3="-1.5% 6% 0% 7.5%"
+          margin4="-2.5% 5% 0 8%"
+        >
           {data[1].description}
         </QuestionDescription>
       </QuestionCard>
@@ -69,7 +77,7 @@ const SectionA = ({
         <QuestionNumber>{data[2].questionNumber}</QuestionNumber>
         <QuestionText>{data[2].question}</QuestionText>
         <Checkbox
-          type='checkbox'
+          type="checkbox"
           id={data[2].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[2].questionNumber - 1]}
@@ -81,7 +89,7 @@ const SectionA = ({
         <QuestionNumber>{data[3].questionNumber}</QuestionNumber>
         <QuestionText>{data[3].question}</QuestionText>
         <Checkbox
-          type='checkbox'
+          type="checkbox"
           id={data[3].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[3].questionNumber - 1]}
@@ -89,17 +97,22 @@ const SectionA = ({
         <QuestionDescription>{data[3].description}</QuestionDescription>
       </QuestionCard>
 
-      <QuestionCard height='143px'>
+      <QuestionCard height="143px" height3="113px" height4="90x">
         <QuestionNumber>{data[4].questionNumber}</QuestionNumber>
         <QuestionText>{data[4].question}</QuestionText>
         <Checkbox
-          type='checkbox'
+          type="checkbox"
           id={data[4].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[4].questionNumber - 1]}
         />
 
-        <QuestionDescription margin1='-1% 3% 2% 9%' margin2='-9.5% 7% 0% 8%'>
+        <QuestionDescription
+          margin1="-1% 3% 2% 9%"
+          margin2="-9.5% 7% 0% 8%"
+          margin3="-1.5% 6% 0% 7.5%"
+          margin4="-2.8% 7% 0 8%"
+        >
           {data[4].description}
         </QuestionDescription>
       </QuestionCard>
@@ -129,7 +142,7 @@ SectionA.propTypes = {
   checkedQuestions: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.bool]))
     .isRequired,
   checkedItems: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.bool]))
-    .isRequired
+    .isRequired,
 };
 
 export default SectionA;
