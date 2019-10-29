2b4fef039608c9e56b9d86ef8c3ed854094326ff
95Rawan <youlostme17@hotmail.com>
2019-10-29 16:28:26 +0200

    Changed the style of the resources layout to be more responsive

--- src/Layout/Resources/index.style.js
+++ src/Layout/Resources/index.style.js
@@ -1,14 +1,28 @@
 import styled from 'styled-components';
 
-export const PageTitle = styled.span`
+export const PageTitle = styled.h1`
   font-size: 1.7rem;
   font-weight: 600;
   color: #205c88;
-  margin: 0% 0% 2% 5%;
+  @media (min-width: 768px) and (max-width: 1024px) {
+    font-size: 3.4rem;
+  }
+  @media (min-width: 1025px) and (max-width: 1280px) {
+    font-size: 3.4rem;
+  }
+  @media (min-width: 1281px) {
+    font-size: 2.3rem;
+  }
 `;
 
 export const ResoursesContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media (min-width: 1281px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
 `;
