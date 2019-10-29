4745371c3f2ddc57c3bce8a5f4513c3a4ed6ecb9
someyaaltous <someyaaltous1995@gmail.com>
2019-10-29 16:02:34 +0200

    changed the styled

--- src/CommonComponents/PrevButton/index.style.js
+++ src/CommonComponents/PrevButton/index.style.js
@@ -2,16 +2,25 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const DivPrev = styled.div`
-display:flex;
-
+  display: flex;
 `;
 
 export const PrevBtn = styled(Link)`
   font-size: 1.9rem;
   color: #e8425c;
   outline: none;
+  -webkit-text-decoration: none;
   text-decoration: none;
-  margin-top: 4%;
-  padding-bottom:1%;
- 
+  padding-bottom: 9%;
+  margin-left: 7%;
+  @media (min-width: 768px) and (max-width: 1024px) {
+    font-size: 2.2rem;
+    margin-left: 14%;
+  }
+  @media (min-width: 1025px) and (max-width: 1280px) {
+    margin-left: 14%;
+  }
+  @media (min-width: 1281px) {
+    margin-left: 18%;
+  }
 `;
