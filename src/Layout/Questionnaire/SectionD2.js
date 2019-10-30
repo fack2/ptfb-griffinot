ba5ee95646acb7da940eff6bf2d29124c008a8be
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-10-30 22:04:14 +0200

    changed the select from checkbox to all div made the all div is working when the user click on anywhere on the div the checkbox is working for section D2

--- src/Layout/Questionnaire/SectionD2.js
+++ src/Layout/Questionnaire/SectionD2.js
@@ -1,12 +1,12 @@
 /* eslint-disable react/jsx-indent */
-import React from 'react';
-import PropTypes from 'prop-types';
-import Popup from 'reactjs-popup';
-import PopupPage from '../../CommonComponents/PopupPage';
-import LevelPop from '../../CommonComponents/LevelPop';
-import LevelData from './QuestionnaireData';
-import data from '../../Data/questionnareData';
-import QuestionnaireData from './QuestionnaireData';
+import React from "react";
+import PropTypes from "prop-types";
+import Popup from "reactjs-popup";
+import PopupPage from "../../CommonComponents/PopupPage";
+import LevelPop from "../../CommonComponents/LevelPop";
+import LevelData from "./QuestionnaireData";
+import data from "../../Data/questionnareData";
+import QuestionnaireData from "./QuestionnaireData";
 import {
   QuestionCard,
   QuestionText,
@@ -15,27 +15,30 @@ import {
   Checkbox,
   QuestionContainer,
   Paragraph,
-  DivNextPrev,
-} from './index.style';
-import PrevButton from '../../CommonComponents/PrevButton';
-import NextButton from '../../CommonComponents/NextButton';
+  DivNextPrev
+} from "./index.style";
+import PrevButton from "../../CommonComponents/PrevButton";
+import NextButton from "../../CommonComponents/NextButton";
 
 const SectionD2 = ({
   checkboxChange,
   checkedQuestions,
   checkedItems,
-  nextButtonClickHandler,
+  nextButtonClickHandler
 }) => (
   <>
     <QuestionContainer>
       <Paragraph>
         Please check the box if your child is able to do the following:
       </Paragraph>
-      <QuestionCard>
+      <QuestionCard
+        id={data[20].questionNumber}
+        onClick={() => checkboxChange(data[20].questionNumber)}
+      >
         <QuestionNumber>{data[20].questionNumber}</QuestionNumber>
         <QuestionText>{data[20].question}</QuestionText>
         <Checkbox
-          type="checkbox"
+          type='checkbox'
           id={data[20].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[20].questionNumber - 1]}
@@ -43,11 +46,15 @@ const SectionD2 = ({
         <QuestionDescription>{data[20].description}</QuestionDescription>
       </QuestionCard>
 
-      <QuestionCard height="90px">
+      <QuestionCard
+        height='90px'
+        id={data[21].questionNumber}
+        onClick={() => checkboxChange(data[21].questionNumber)}
+      >
         <QuestionNumber>{data[21].questionNumber}</QuestionNumber>
         <QuestionText>{data[21].question}</QuestionText>
         <Checkbox
-          type="checkbox"
+          type='checkbox'
           id={data[21].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[21].questionNumber - 1]}
@@ -55,11 +62,14 @@ const SectionD2 = ({
         <QuestionDescription>{data[21].description}</QuestionDescription>
       </QuestionCard>
 
-      <QuestionCard>
+      <QuestionCard
+        id={data[22].questionNumber}
+        onClick={() => checkboxChange(data[22].questionNumber)}
+      >
         <QuestionNumber>{data[22].questionNumber}</QuestionNumber>
         <QuestionText>{data[22].question}</QuestionText>
         <Checkbox
-          type="checkbox"
+          type='checkbox'
           id={data[22].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[22].questionNumber - 1]}
@@ -67,11 +77,14 @@ const SectionD2 = ({
         <QuestionDescription>{data[22].description}</QuestionDescription>
       </QuestionCard>
 
-      <QuestionCard>
+      <QuestionCard
+        id={data[23].questionNumber}
+        onClick={() => checkboxChange(data[23].questionNumber)}
+      >
         <QuestionNumber>{data[23].questionNumber}</QuestionNumber>
         <QuestionText>{data[23].question}</QuestionText>
         <Checkbox
-          type="checkbox"
+          type='checkbox'
           id={data[23].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[23].questionNumber - 1]}
@@ -79,11 +92,14 @@ const SectionD2 = ({
         <QuestionDescription>{data[23].description}</QuestionDescription>
       </QuestionCard>
 
-      <QuestionCard>
+      <QuestionCard
+        id={data[24].questionNumber}
+        onClick={() => checkboxChange(data[24].questionNumber)}
+      >
         <QuestionNumber>{data[24].questionNumber}</QuestionNumber>
         <QuestionText>{data[24].question}</QuestionText>
         <Checkbox
-          type="checkbox"
+          type='checkbox'
           id={data[24].questionNumber}
           onChange={checkboxChange}
           checked={checkedQuestions[data[24].questionNumber - 1]}
@@ -92,19 +108,19 @@ const SectionD2 = ({
       </QuestionCard>
       <DivNextPrev>
         <PrevButton
-          prevLink="/questionnaire"
+          prevLink='/questionnaire'
           nextButtonClickHandler={nextButtonClickHandler}
         />
 
         {checkedItems.length === QuestionnaireData[5].limit ? (
-          <Popup modal trigger={<NextButton Margin="55rem" />}>
+          <Popup modal trigger={<NextButton Margin='55rem' />}>
             <PopupPage
-              description="Our programme will be too simple for your child and we would not recommend it.  It is likely your child doesn’t need extra support with developing their fine motor skills."
-              NextLink="/"
+              description='Our programme will be too simple for your child and we would not recommend it.  It is likely your child doesn’t need extra support with developing their fine motor skills.'
+              NextLink='/'
             />
           </Popup>
         ) : (
-          <Popup modal trigger={<NextButton Margin="55rem" />}>
+          <Popup modal trigger={<NextButton Margin='55rem' />}>
             <LevelPop
               levelScore={LevelData[5].LevelNo}
               description={LevelData[5].uncompletedMsg}
@@ -123,7 +139,7 @@ SectionD2.propTypes = {
   checkedQuestions: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.bool]))
     .isRequired,
   checkedItems: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.number]))
-    .isRequired,
+    .isRequired
 };
 
 export default SectionD2;
