8497b6a329e0111d057ddc1a36829e2898eb6d59
sara219 <whitenoise6014@gmail.com>
2019-10-30 20:40:16 +0200

    Added the close button for both pop-up

--- src/Layout/Questionnaire/Option3.js
+++ src/Layout/Questionnaire/Option3.js
@@ -20,11 +20,12 @@ import {
   Para,
   LevelDiv,
   ToLink,
+  Close,
 } from './index.style';
 
 import PrevButton from '../../CommonComponents/PrevButton';
 
-const Option3 = ({ nextButtonClickHandler }) => (
+const Option3 = ({ nextButtonClickHandler, close }) => (
   <>
     <TitleOption>
       What skill do you feel your child needs the most help with improving, at
@@ -41,41 +42,46 @@ const Option3 = ({ nextButtonClickHandler }) => (
           </Option>
         )}
       >
-        <BodyPage>
-          <PopUPCard>
-            <PopUpBox Height="110rem">
-              <Logo>
-                <LogoImage src={popUpImg} alt="a kid holding a kite logo" />
-              </Logo>
-              <Container2>
-                <LevelIntro>
-                  The most appropriate programme for your child is:
-                </LevelIntro>
-                <LevelDiv>
-                  <CircleLevel>
-                    <Para> Supporting Pencil Grasp Development </Para>
-                  </CircleLevel>
-                </LevelDiv>
-                <PopUpDiv FontSize="2rem">
-                  The Supporting Pencil Grasp Development Programme is
-                  specifically designed to help children improve their pencil
-                  grasp. The programme consists of two warm ups, 14 fine motor
-                  activities and 14 worksheet tasks. The activities focus on
-                  improving the hand dexterity required to hold a pencil. The
-                  worksheets help with pencil control. By the end of the
-                  programme your child should be able to hold their pencil using
-                  an adequate grasp This might be a tripod, modified tripod or
-                  quadruped grasp. They should also have made improvements with
-                  their pencil control.
-                </PopUpDiv>
+        {(close) => (
+          <BodyPage>
+            <PopUPCard>
+              <PopUpBox Height="81%">
+                <Close onClick={close}>
+                  &times;
+                </Close>
+                <Logo>
+                  <LogoImage src={popUpImg} alt="a kid holding a kite logo" />
+                </Logo>
+                <Container2>
+                  <LevelIntro>
+                    The most appropriate programme for your child is:
+                  </LevelIntro>
+                  <LevelDiv>
+                    <CircleLevel>
+                      <Para> Supporting Pencil Grasp Development </Para>
+                    </CircleLevel>
+                  </LevelDiv>
+                  <PopUpDiv FontSize="2rem">
+                    The Supporting Pencil Grasp Development Programme is
+                    specifically designed to help children improve their pencil
+                    grasp. The programme consists of two warm ups, 14 fine motor
+                    activities and 14 worksheet tasks. The activities focus on
+                    improving the hand dexterity required to hold a pencil. The
+                    worksheets help with pencil control. By the end of the
+                    programme your child should be able to hold their pencil using
+                    an adequate grasp This might be a tripod, modified tripod or
+                    quadruped grasp. They should also have made improvements with
+                    their pencil control.
+                  </PopUpDiv>
 
-                <ToLink href="https://griffinot.teachable.com/p/online-pencil-grasp-development-programme1">
-                  Purchase programme
-                </ToLink>
-              </Container2>
-            </PopUpBox>
-          </PopUPCard>
-        </BodyPage>
+                  <ToLink href="https://griffinot.teachable.com/p/online-pencil-grasp-development-programme1">
+                    Purchase programme
+                  </ToLink>
+                </Container2>
+              </PopUpBox>
+            </PopUPCard>
+          </BodyPage>
+        )}
       </Popup>
 
       {/* 2nd option  */}
@@ -94,12 +100,15 @@ const Option3 = ({ nextButtonClickHandler }) => (
           </Option>
         )}
       >
-        <PopupPage
-          description="This programme will be launching in March 2020, please consider joining our mailing list for updates"
-          optionLink="https://www.griffinot.com/mailing-list-sign-up/"
-          optionText="Our Mailing list"
-          NextLink="/"
-        />
+        {(close) => (
+          <PopupPage
+            close={close}
+            description="This programme will be launching in March 2020, please consider joining our mailing list for updates"
+            optionLink="https://www.griffinot.com/mailing-list-sign-up/"
+            optionText="Our Mailing list"
+            NextLink="/"
+          />
+        )}
       </Popup>
     </DivData>
 
