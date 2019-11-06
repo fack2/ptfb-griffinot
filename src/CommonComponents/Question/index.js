1014fbfb47bd09bbebf9263e55762d5aa908bc28
95Rawan <youlostme17@hotmail.com>
2019-11-06 10:46:37 +0200

    Changed style import path

--- src/CommonComponents/Question/index.js
+++ src/CommonComponents/Question/index.js
@@ -5,16 +5,16 @@ import {
   QuestionDescription,
   QuestionNumber,
   Checkbox,
-} from '../../Layout/Questionnaire/index.style';
+} from './index.style';
 
-const Question = ({ checkboxChange, checkedQuestions, data }) => data.map((QuestionsData, index, data) => (
+const Question = ({ checkboxChange, checkedQuestions, data }) => data.map((QuestionsData, index, dataArray) => (
   <QuestionCard
     id={QuestionsData.questionNumber}
     onClick={() => checkboxChange(QuestionsData.questionNumber)}
   >
     <QuestionNumber>{QuestionsData.questionNumber}</QuestionNumber>
     <QuestionText>{QuestionsData.question}</QuestionText>
-    <Checkbox type="checkbox" checked={checkedQuestions[data[index].questionNumber - 1]} />
+    <Checkbox type="checkbox" checked={checkedQuestions[dataArray[index].questionNumber - 1]} />
     <QuestionDescription>{QuestionsData.description}</QuestionDescription>
   </QuestionCard>
 ));
